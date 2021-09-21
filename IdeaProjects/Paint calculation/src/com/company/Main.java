c03056ab9fe2e86ccc8bdd78f49c5cee8a162560
Euan Hutcheson <euanhutcheson@Euans-MacBook-Pro.local>
2021-09-21 10:52:24 +0100

    first commit

---
+++ IdeaProjects/Paint calculation/src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+     static void wallsmethod() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("What is the width of all of the walls in the room added(in meters)?");
+
+        float width = myObj.nextFloat();
+
+        System.out.println("What is the height of the room(in meters)?");
+
+        float height = myObj.nextFloat();
+
+
+        System.out.println("How much paint is in the container?(in litres)");
+
+        float litres = myObj.nextFloat();
+        float area = width * height;
+        float arealitres = area / litres;
+        float numberoflitres = arealitres / 12;
+        float numberofpaintcans = numberoflitres / litres;
+        double rounded = Math.ceil(numberofpaintcans / 100);
+        if (rounded == 1) {
+            System.out.println(rounded);
+            System.out.println("paint can");
+        }else {
+            System.out.println(rounded);
+            System.out.println("paint cans");
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args)
+            {
+                wallsmethod();
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ IdeaProjects/helloworld/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String [] args)
+    {
+	 System.out.println("Hello World");
+
+     int x = 5;
+     int y = 6;
+
+     System.out.println(x*y);
+
+     float myFloat = 5.65f;
+
+     System.out.println(myFloat);
+     double myDouble= 5.65d;
+     System.out.println(myDouble);
+
+     long myLong = 40000l;
+     System.out.println(myLong);
+
+     double myInt = myLong;
+     System.out.println(myInt);
+
+     boolean myBool1 = true;
+     boolean myBool2= 12> 10;
+     System.out.println(myBool2);
+
+
+
+     int myNum= (int) myDouble;
+     System.out.println(myNum);
+
+     System.out.println(myNum+1);
+
+
+
+    }
+
+
+
+}
+
