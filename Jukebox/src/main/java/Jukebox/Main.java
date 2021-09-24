c0afb0b61c628a6b87b3508b1fe17b4ccf0779c6
Euan Hutcheson <euanhutcheson@Euans-MacBook-Pro.local>
2021-09-24 12:49:06 +0100

    sonarcludcommit

--- Jukebox/src/main/java/Jukebox/Main.java
+++ Jukebox/src/main/java/Jukebox/Main.java
@@ -5,51 +5,37 @@ import java.util.Scanner;
 
 public class Main {
 public static void main(String [] arg) {
-    Scanner myMusic = new Scanner(System.in);
-    Scanner mySong = new Scanner(System.in);
-    Album a = new Album();
-    Fleetwood f = new Fleetwood();
-    Kanye ka = new Kanye();
-    arctic am = new arctic();
-    List<String> aname= a.albumname();
-    List<String> genre= a.genretype();
-    List<Integer> syear= a.songyear();
-    List<String> artname= a.artistname();
-    List<String> songn1= f.songs1();
-    List<String> songn2= ka.songs2();
-    List<String> songn3= am.songs3();
+    UserInteraction u= new UserInteraction();
     System.out.println("Which album would you like to listen to?");
-    System.out.println("1." +aname.get(0));
-    System.out.println("2." +aname.get(1));
-    System.out.println("3." +aname.get(2));
-    int myalbum= myMusic.nextInt();
-    System.out.println("Information:");
-    System.out.println("Genre:"+genre.get(myalbum-1));
-    System.out.println("Year:"+ syear.get(myalbum-1));
-    System.out.println("Artist:"+artname.get(myalbum-1));
+    System.out.println("1." +u.UIalbum().get(0));
+    System.out.println("2." +u.UIalbum().get(1));
+    System.out.println("3." +u.UIalbum().get(2));
+    System.out.println("Genre:"+u.UIgenre().get(u.UIchoice1(myalbum)-1));
+    System.out.println("Year:"+ u.UIsongyear().get(u.UIchoice1(myalbum)-1));
+    System.out.println("Artist:"+u.UIartistname().get(u.UIchoice1(myalbum)-1));
 
-    if (myalbum == 1) {
-            System.out.println("Which track would you like to listen to?(1-11)");
-            for (int i = 0; i <= (songn1.size() - 1); i++) {
-                System.out.println(songn1.get(i));
-            }
-            int mysong1 = mySong.nextInt();
-            System.out.println("Now Playing:" + songn1.get(mysong1 - 1));
-        } else if (myalbum == 2) {
-            System.out.println("Which track would you like to listen to?(1-13");
-            for (int i = 0; i <= (songn2.size() - 1); i++) {
-                System.out.println(songn2.get(i));
-            }
-            int mysong2 = mySong.nextInt();
-            System.out.println("Now Playing:" + songn2.get(mysong2 - 1));
-        } else if (myalbum == 3) {
-            System.out.println("Which track would you like to listen to?");
-            for (int i = 0; i <= songn3.size() - 1; i++) {
-                System.out.println(songn3.get(i));
-            }
-            int mysong3 = mySong.nextInt();
-            System.out.println("Now Playing:" + songn3.get(mysong3 - 1));
-        }
+    //if (myalbum == 1) {
+      //      System.out.println("Which track would you like to listen to?(1-11)");
+        //    for (int i = 0; i <= (songn1.size() - 1); i++) {
+          //      System.out.println(songn1.get(i));
+            //}
+            //int mysong1 = mySong.nextInt();
+            //System.out.println("Now Playing:" + songn1.get(mysong1 - 1));
+        //} else if (myalbum == 2) {
+          //  System.out.println("Which track would you like to listen to?(1-13");
+            //for (int i = 0; i <= (songn2.size() - 1); i++) {
+            //    System.out.println(songn2.get(i));
+            //}
+            //int mysong2 = mySong.nextInt();
+           // System.out.println("Now Playing:" + songn2.get(mysong2 - 1));
+      //  } else if (myalbum == 3) {
+        //    System.out.println("Which track would you like to listen to?");
+          //  for (int i = 0; i <= songn3.size() - 1; i++) {
+            //    System.out.println(songn3.get(i));
+        //    }
+          //  int mysong3 = mySong.nextInt();
+          //  System.out.println("Now Playing:" + songn3.get(mysong3 - 1));
+        //}
     }
 
 
---
+++ Jukebox/src/main/java/Jukebox/UserInteraction.java
@@ -0,0 +1,56 @@
+package Jukebox;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInteraction {
+    public int UIchoice1(){
+        Scanner myMusic = new Scanner(System.in);
+        int myalbum = myMusic.nextInt();
+
+        return myalbum}
+
+    public List<String> UIalbum() {
+   // Scanner myMusic = new Scanner(System.in);
+   // Scanner mySong = new Scanner(System.in);
+    Album a = new Album();
+    List<String> aname = a.albumname();
+
+    return aname;}
+
+
+    public List<String> UIgenre(){
+        // Scanner mySong = new Scanner(System.in);
+    Album a = new Album();
+    List<String> genre = a.genretype();
+
+    return genre;}
+
+
+
+    public List<Integer> UIsongyear(){
+        Album a = new Album();
+        List<Integer> syear = a.songyear();
+
+        return syear;}
+
+    public List<String> UIartistname(){
+        Album a = new Album();
+        List<String> artname = a.artistname();
+
+        return artname;}
+
+
+    }
+
+
+   // List<String> artname = a.artistname();
+   // List<String> songn1 = f.songs1();
+   // List<String> songn2 = ka.songs2();
+    //List<String> songn3 = am.songs3();
+    //int myalbum = myMusic.nextInt();
+
+
+
+
+
